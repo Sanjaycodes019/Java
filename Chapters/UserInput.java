bb50203afa58f161284bdea54c8bb42b2e4d407a
Sanjay Gupta <166055466+Sanjaycodes019@users.noreply.github.com>
2025-07-04 04:46:27 +0000

    commit

---
+++ Chapters/UserInput.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class UserInput{
+    public static void main(String args[]){
+        System.out.println("Enter an integer: ");
+        int intValue = sc.nextInt();
+        
+    }
+}
